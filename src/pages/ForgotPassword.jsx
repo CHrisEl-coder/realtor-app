f0b40f5125e73c466dc34954ed0dfa485ebc8285
CHrisEl-coder <edwinchristian97@gmail.com>
2025-04-15 02:04:41 -0700

    Implement The Reset Password Functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,45 +1,80 @@
-import React from "react"
-import { useState } from "react"
-import { BiUserCircle } from 'react-icons/bi'
-import { BsEnvelopeAt } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
-import Oauth from "../components/Oauth"
+import React from "react";
+import { useState } from "react";
+import { BiUserCircle } from "react-icons/bi";
+import { BsEnvelopeAt } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Oauth from "../components/Oauth";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
 
-export default function SignIn () {
-    const [email, setEmail] = useState("")
+export default function SignIn() {
+  const [email, setEmail] = useState("");
 
- 
-
-   function onChange(e) {
+  function onChange(e) {
     setEmail(e.target.value);
-   }
+  }
 
-   
-    return (
-                
-        <section className="sign-sec">
-            <div className="sign-main">
-            <BiUserCircle id="head-icon" className=""/>
-                <div className="form">
-                    <form className="fm space-y-10">
-                        <div className="mail">
-                            <input type='email' id='email' placeholder="Enter Your Email" value={email} className="w-full" onChange={onChange}/>
-                            <BsEnvelopeAt className="icon"/>
-                        </div>
-                        <div className="flex flex-col justify-center items-center space-y-3">
-                            <button type="submit" className="bg-[#010130] py-2 px-16 rounded text-white font-semi-bold hover:bg-transparent hover:border-2 transition ease-in-out duration-300">Reset Password</button>
+  const resetPassword = async (e) => {
+    e.preventDefault();
 
-                            <div className="flex before:border-t before:flex-1 items-center after:border-t after:flex-1 w-full mx-4"><p className="text-white font-semibold text-center mx-2">OR</p></div>
+    try {
+      const auth = getAuth();
 
-                            <Oauth/>
-                        </div>
-                    </form>
-                    <div className="flex justify-around text-blue-500 font-semibold lg:text-[0.875rem] text-[0.75rem] my-4">
-                        <Link to="/sign-up" className="hover:text-blue-950 transition ease-in-out duration-200 shadow-md hover:shadow-lg active:scale-110">Sign-Up</Link>
-                        <Link to="/sign-in" className="hover:text-blue-950 transition ease-in-out duration-200 shadow-md hover:shadow-lg">Log-In</Link>
-                    </div>
-                </div>
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Reset password link sent to your email");
+    } catch (error) {
+      toast.error("Could not send reset password link");
+    }
+  };
+
+  return (
+    <section className="sign-sec">
+      <div className="sign-main">
+        <BiUserCircle id="head-icon" className="" />
+        <div className="form">
+          <form className="fm space-y-10" onClick={resetPassword}>
+            <div className="mail">
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter Your Email"
+                value={email}
+                className="w-full"
+                onChange={onChange}
+              />
+              <BsEnvelopeAt className="icon" />
             </div>
-        </section>
-    )
+            <div className="flex flex-col justify-center items-center space-y-3">
+              <button
+                type="submit"
+                className="bg-[#010130] py-2 px-16 rounded text-white font-semi-bold hover:bg-transparent hover:border-2 transition ease-in-out duration-300"
+              >
+                Reset Password
+              </button>
+
+              <div className="flex before:border-t before:flex-1 items-center after:border-t after:flex-1 w-full mx-4">
+                <p className="text-white font-semibold text-center mx-2">OR</p>
+              </div>
+
+              <Oauth />
+            </div>
+          </form>
+          <div className="flex justify-around text-blue-500 font-semibold lg:text-[0.875rem] text-[0.75rem] my-4">
+            <Link
+              to="/sign-up"
+              className="hover:text-blue-950 transition ease-in-out duration-200 shadow-md hover:shadow-lg active:scale-110"
+            >
+              Sign-Up
+            </Link>
+            <Link
+              to="/sign-in"
+              className="hover:text-blue-950 transition ease-in-out duration-200 shadow-md hover:shadow-lg"
+            >
+              Log-In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
